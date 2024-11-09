9e38f13205794739f8807b50c46e48cfe9fe1f37
shale <slucas001@citymail.cuny.edu>
2024-11-09 00:55:29 -0500

    added the click and drag stuff, NEED a refactor soon!

--- main.cc
+++ main.cc
@@ -26,21 +26,48 @@ void f () {
 	float y_vel = 0;
 	float gravity = 9.81;
 	float time_delta = 0.1;
+	bool dragging = false;
+
+	int mouse_off_x = 0;
+	int mouse_off_y = 0;
+	
     while (running) {
         SDL_Event ev;
         while (SDL_PollEvent(&ev)) {
             if (SDL_QUIT == ev.type) {
                 running = false;
                 break;
-            }
+            } else if (ev.type == SDL_MOUSEBUTTONDOWN) {
+				int x_mouse_pos = ev.button.x;
+				int y_mouse_pos = ev.button.y;
+
+				if (x_mouse_pos >= box.x && x_mouse_pos <= box.x + box.w &&
+					y_mouse_pos >= box.y && y_mouse_pos <= box.y + box.h) {
+			
+					dragging = true;
+					y_vel = 0;	
+
+					mouse_off_x = x_mouse_pos - box.x;
+					mouse_off_y = y_mouse_pos - box.y;
+				}	
+	
+			} else if (ev.type == SDL_MOUSEBUTTONUP) {
+				dragging = false;
+			} else if (ev.type == SDL_MOUSEMOTION && dragging) {
+				box.x = ev.motion.x - mouse_off_x;	
+				box.y = ev.motion.y - mouse_off_y;	
+			}
         }
 
-		y_vel += gravity * time_delta;
-		box.y += static_cast<int>(y_vel);
 
-		if (box.y + box.h > 480) {
-			box.y = 480 - box.h;
-			y_vel = 0;
+		if (!dragging) {
+			y_vel += gravity * time_delta;
+			box.y += static_cast<int>(y_vel);
+
+			if (box.y + box.h > 480) {
+				box.y = 480 - box.h;
+				y_vel = 0;
+			}
 		}
 
         SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
