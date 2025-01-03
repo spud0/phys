4f4ac140208908bebc01353c9f081c29a8c40829
shale <slucas001@citymail.cuny.edu>
2025-01-03 00:53:27 -0500

    changed the old stuff, to better defn for a quadtree, keeping things simple

--- src/util.hh
+++ src/util.hh
@@ -1,10 +1,28 @@
+#pragma once 
+#include <cstdint>
+#include <vector>
+
+#include "math.hh"
+
+#define L 2 
+#define W L
+
 namespace util {
 
-	template <typename point>
-	class quadtree {
-	  public:
+	using node_id = std::uint32_t;
+	static constexpr node_id empty = -1; 
+
+	// Temporary can store more information in here...
+	struct node {	
+		node_id children[L][W] = {
+			{empty, empty}, {empty, empty}
+		};
+	};
 	
-	  private:
-		
+
+	struct quadtree {
+		math::box bbox;	
+		node_id root;
+		std::vector<node> nodes;
 	};
 }
